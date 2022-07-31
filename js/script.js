5f6c9f607b3ed7bfbce49f1f10231ac1745252ef
Arnaldo Reyes <arnaldoreyes13@gmail.com>
2022-07-31 14:19:05 -0400

    funcionalidad del search

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#search').on('submit', function(e){
+        e.preventDefault();
+
+        if ($("#correo").val()!='' &&  $("#correo").val()!=null) {
+            location.href="https://www.google.com/search?q="+$('#correo').val();
+        }
+        
+        else{
+            $("#correo").addClass('is-invalid');
+        }
+    });
+});
